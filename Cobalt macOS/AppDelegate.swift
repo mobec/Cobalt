3df4da8ee07f40ed617ab7d980242b713c3db21d
Mo Becher <gitkraken@mo-becher.de>
2018-06-19 11:22:53 +0200

    Initial Commit

---
+++ Cobalt macOS/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Cobalt macOS
+//
+//  Created by Mo Becher on 19.06.18.
+//  Copyright © 2018 Mo Becher. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
